8877dfafeb0778887cfbeab37959f2fd0bcfb09e
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-24 18:25:48 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const server = express();
+
+const dev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+server.use('/graphql', graphqlHTTP((req, res, gql) => ({
+  schema,
+  graphiql: dev,
+  pretty: dev,
+})));
+
+server.listen(PORT, error => {
+  if (error) throw error;
+
+  console.log(`> Ready on Port ${PORT}`);
+});
