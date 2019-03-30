da9e4c427f91b7b7f317fb6d85541b3e30186fc7
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-30 20:25:55 +0100

    added basic authorization

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ require('dotenv').config();
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+let schema = require('./schema');
 
 const server = express();
 
 const dev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
+server.use('/login', (req, res) => {
+  res.json({
+    id: 1,
+    username: 'admin',
+    jwt: jwt.sign(
+      {
+        id: 1,
+        username: 'admin',
+      },
+      'testest',
+      { expiresIn: 60 * 60 },
+    ),
+  });
+});
+
 server.use(
   '/graphql',
+  expressJwt({ secret: 'testest' }),
   graphqlHTTP((req, res, gql) => ({
     schema,
     graphiql: dev,
