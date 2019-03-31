26b15ecf4739867bd6d3229b943c69cef4fb4d8f
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-31 12:23:06 +0200

    updated env variables

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
+require('dotenv').config();
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const PORT = process.env.PORT || 5000;
+
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 const resolvers = mergeResolvers(
   fileLoader(path.join(__dirname, './resolvers')),
@@ -20,8 +24,10 @@ const endpointURL = '/graphql';
 
 app.use(endpointURL, bodyParser.json(), graphqlExpress({ schema }));
 
-app.use('/graphiql', graphiqlExpress({ endpointURL }));
+if (!IS_PRODUCTION) {
+  app.use('/graphiql', graphiqlExpress({ endpointURL }));
+}
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 8080'); // eslint-disable-line
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`); // eslint-disable-line
 });
