6ed5a74ec6e35033c5c5a975ae8933d7b161cf5b
FlorianWoelki <FlorianWoelki@gmx.de>
2019-04-02 09:32:42 +0200

    updated integration tests

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -52,22 +52,95 @@ describe('user resolvers', () => {
     });
   });
 
+  let authToken = '';
+
   test('login', async () => {
     const response = await axios.post('http://127.0.0.1:8080/graphql', {
       query: `
         mutation {
           login(email:"test@test.de", password:"123456") {
             email
+            token
           }
         }
       `,
     });
 
     const { data } = response;
+    authToken = data.data.login.token;
+
+    data.data.login.token = null;
     expect(data).toMatchObject({
       data: {
         login: {
           email: 'test@test.de',
+          token: null,
+        },
+      },
+    });
+  });
+
+  // Tests not working, maybe adding CORS will work...
+  test('addAddress', async () => {
+    const response = await axios.post('http://127.0.0.1:8080/graphql', {
+      query: `
+        addAddress(country:"germany", city:"berlin", street:"teststreet", zipCode:12622) {
+          country
+          city
+          street
+          zipCode
+          streetExtra
+        }
+      `,
+    },
+    {
+      headers: {
+        authorization: `Bearer ${authToken}`,
+      },
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        addAddress: {
+          country: 'germany',
+          city: 'berlin',
+          street: 'teststreet',
+          zipCode: 12622,
+          streetExtra: null,
+        },
+      },
+    });
+  });
+
+  test('getAddress', async () => {
+    const response = await axios.post('http://127.0.0.1:8080/graphql', {
+      authorization: `Bearer ${authToken}`,
+      query: `
+        getAddress() {
+          city
+          country
+          street
+          zipCode
+          streetExtra
+        }
+      `,
+    },
+    {
+      headers: {
+        authorization: `Bearer ${authToken}`,
+      },
+    });
+
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        addAddress: {
+          country: 'germany',
+          city: 'berlin',
+          street: 'teststreet',
+          zipCode: 12622,
+          streetExtra: null,
         },
       },
     });
